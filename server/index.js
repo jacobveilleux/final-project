24010029aa7fff9b34cca1d8d2616ba32dd4bd8b
jacobveilleux <jacob.veilleux@hotmail.com>
2021-09-27 13:17:18 -0400

    create server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// import the needed node_modules.
+const express = require("express");
+const morgan = require("morgan");
+
+express()
+    // chain express methods for convenience
+    .use(morgan("tiny"))
+    .use(express.json())
+    // Any requests for static files will go into the public folder
+    .use(express.static("public"))
+
+    //ENDPOINTS
+    // ---------------------------------
+    // add new endpoints here 👇
+    // ---------------------------------
+
+    // this is our catch all endpoint.
+    .get("*", (req, res) => {
+        res.status(404).json({
+            status: 404,
+            message: "This is obviously not what you are looking for.",
+        });
+    })
+
+    // Node spins up our server and sets it to listen on port 8000.
+    .listen(8000, () => console.log(`Listening on port 8000`));
