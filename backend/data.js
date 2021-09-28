86b9c6ac479c533a85d0b2d85ced427718270bf4
jacobveilleux <jacob.veilleux@hotmail.com>
2021-09-28 12:17:50 -0400

    fix data

--- backend/data.js
+++ backend/data.js
@@ -2,22 +2,22 @@
 // 3 categories: moto, atv, snowmobile
 
 const owners = [
-  {
-    id: "1000",
-    name: "Jacob",
-    surname: "Veilleux",
-    email: "jv@mail.com",
-    category: "moto",
-  },
+    {
+        _id: "1000",
+        name: "Jacob",
+        surname: "Veilleux",
+        email: "jv@mail.com",
+        category: "moto",
+    },
 ];
 
 const riders = [
-  {
-    id: "5000",
-    name: "Rider",
-    surname: "One",
-    email: "riderone@mail.com",
-  },
+    {
+        _id: "5000",
+        name: "Rider",
+        surname: "One",
+        email: "riderone@mail.com",
+    },
 ];
 
 module.exports = { owners, riders };
